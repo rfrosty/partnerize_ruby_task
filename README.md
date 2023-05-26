f3a9c9f3af8657d9514677956d0a57a8392a2d88
Ryan <ryanfrost2015@gmail.com>
2023-05-26 09:48:35 +0100

    adding files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # partnerize_ruby_task
+
+Install ruby. Execute `ruby sentence_generator.rb`
---
+++ sentence_generator.rb
@@ -0,0 +1,32 @@
+require 'net/http'
+require 'json'
+
+def return_resource(resource)
+  url = "https://swapi.dev/api/#{resource}/#{rand(1..15)}/"
+  uri = URI(url)
+  response = Net::HTTP.get(uri)
+  JSON.parse(response)
+end
+
+def return_character
+  character_info = return_resource('people')
+  gender = character_info['gender']
+  title = gender == 'female' ? 'Ms.' : 'Mr.'
+  surname = character_info['name'].split(' ').last
+  "#{title} #{surname}"
+end
+
+def return_starship
+  starship_info = return_resource('starships')
+  if starship_info.key?('name')
+    starship_info['name']
+  else
+    return_starship
+  end
+end
+
+character1 = return_character
+character2 = return_character
+starship = return_starship
+
+puts "#{character1} and #{character2} cruising around in their #{starship}"
